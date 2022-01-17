590c4f1d1fa3e1081ea5d9f9e8236d76074928a3
lsmith32 <lmksmith97@gmail.com>
2022-01-16 19:24:36 -0600

    search city bar added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const APIKey = "c09bae87a6bcc05461fb40cbd7520587"
+    const inputEl = document.getElementById("input-city");
+    const searchEl = document.getElementById("search-button");
+
+//API key
+    const APIKey = "c09bae87a6bcc05461fb40cbd7520587"
+
+//log city name typed by user
+    searchEl.addEventListener("Click", function() {
+        console.log(inputEl);
+    });
